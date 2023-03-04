abc6a325cbc312d7cdf954c10d0b951b03c5d26f
star <1598624985@qq.com>
2023-03-04 15:54:52 +0800

    fix(baidu-search): change content

--- js/baidu-search.js
+++ js/baidu-search.js
@@ -4,8 +4,10 @@
   * 1. all-去除百度搜索的广告
 */
 
+
 const listener_config = {attributes: true, childList: true, subtree: true};
-const observer = new MutationObserver(() => {
+
+let observer = new MutationObserver(() => {
     document.querySelectorAll(".EC_result").forEach(e => {
         e.parentElement.parentElement.parentElement.remove();
     })
@@ -14,11 +16,35 @@ const observer = new MutationObserver(() => {
     })
 })
 
+function start_listen() {
+    let listen_content = document.querySelector("body");
+    if (listen_content) {
+        observer.observe(listen_content, listener_config);
+        return true;
+    }
+
+    return false;
+}
+
+let leave_homepage_observer = new MutationObserver(() => {
+    if (window.location.href.indexOf("/s?") === -1) {
+        return;
+    }
+
+    if (start_listen()) {
+        leave_homepage_observer?.disconnect();
+    }
+})
+
 window.onload = function () {
-    const listen_body = document.querySelector("#content_left");
-    observer.observe(listen_body, listener_config);
+    if (window.location.href.indexOf("/s?") !== -1) {
+        start_listen();
+    }
+    let listen_body = document.querySelector("body");
+    leave_homepage_observer.observe(listen_body, listener_config);
 }
 
 window.onbeforeunload = function () {
+    leave_homepage_observer?.disconnect();
     observer.disconnect();
 }
